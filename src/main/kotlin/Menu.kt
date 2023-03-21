5f0fd455779e212c556550e44d189b8c0e8cbf95
oleger2008 <otsoy1995@yandex.ru>
2023-03-22 02:29:16 +0300

    добавил базовый класс менюхи

---
+++ src/main/kotlin/Menu.kt
@@ -0,0 +1,29 @@
+abstract class Menu(
+    val title: String,
+    val createCmdName: String
+) {
+    abstract val exit: () -> Unit
+
+    protected abstract fun create()
+    protected abstract fun showImpl()
+    protected abstract fun executeImpl(index: Int)
+
+    fun execute(cmdIdx: Int) {
+        when (cmdIdx) {
+            0 -> exit()
+            1 -> create()
+            else -> executeImpl(cmdIdx - 2)
+        }
+    }
+
+    fun show() {
+        println(title)
+        println("0. Выход")
+        println("1. $createCmdName")
+        showImpl()
+    }
+
+    protected fun printWrongCmdIdxMsg() {
+        println("Введен некорректный номер команды. Повторите попытку.\n")
+    }
+}
